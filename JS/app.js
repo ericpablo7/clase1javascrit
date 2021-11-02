369e4b958b7d5c52de354b978295a2db1d8baee4
Eric Internicola <pablointerflet@gmail.com>
2021-11-01 22:42:46 -0300

    [ADD] se hicieron cambios en js

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,60 @@
-alert("Cual es tu top5 de peliculas favoritas? ")
 
+alert("PLATAFORMA CODERHOUSE")
+alert("Vamos a registrar a un nuevo alumno")
 
-for (let i = 1; i <= 5; i++) {
-   
-    let ingresarPelicula = prompt("Ingresar pelicula");
-    
-    document.write(` TOP  N° ${i} ${ingresarPelicula} /`);
+let nombre = prompt("Ingrese su nombre: ")
+let apellido = prompt("Ingrese su apellido: ")
+saludar(nombre, apellido)
+
+let edad = parseInt(prompt("Edad: "))
+
+while(edad < 18){
+    alert("El dato es incorrecto , ingrese nuevamente.")
+    edad = parseInt(prompt("Edad: "))
 }
+
+function saludar(nombre, apellido){
+    alert(`Bienvenido alumno: ${nombre} ${apellido}`)
+}
+
+let primerNota = parseInt(prompt("Ingrese la primer nota: "))
+let segundaNota = parseInt(prompt("Ingrese la segund anota: "))
+
+let sumoNotas = (a,b) => a + b
+
+let promedio = function (primerNota, segundaNota) { return sumoNotas(primerNota, segundaNota) / 2}
+
+alert("El promedio de notas es de " + promedio(primerNota,segundaNota))
+
+if(promedio(primerNota, segundaNota) < 6) {
+    alert("Lamentablemente desaprobaste, tenes que repetir el curso")
+}
+else{
+    alert("Felicitaciones , aprobaste el curso")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
